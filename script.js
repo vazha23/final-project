ea3f5e72ada7713445691b42d5083fb4786dade8
vazha23 <vajamaxarashvili2@gmail.com>
2023-01-21 16:17:03 +0400

    final-project
     i made final project about softwere agency

---
+++ script.js
@@ -0,0 +1,22 @@
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
